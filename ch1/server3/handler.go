9cbe29a5b0d707bab796a1baaa404d3afac8ba17
qnl3 <brown.anthony.t@gmail.com>
2017-12-21 09:37:19 -0500

    cloned server2

---
+++ ch1/server3/handler.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}

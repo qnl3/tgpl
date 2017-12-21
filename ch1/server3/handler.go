c3f938a7b3d9e288bb450a549e832eeb5fe6a836
qnl3 <brown.anthony.t@gmail.com>
2017-12-21 09:48:46 -0500

    server3 incomplete

--- ch1/server3/handler.go
+++ ch1/server3/handler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -9,5 +8,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	count++
 	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	log.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	log.Fprintf(s, "%s %s %s\n", r.Method, r.Url, r.Proto)
+
+	for k, v := range r.Header {
+		log.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
 }
